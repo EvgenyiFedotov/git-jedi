e2cb3f8929aa530085277211bb0fad4e72156907
e.fedotov <fedotov1290@gmail.com>
2020-01-08 21:28:31 +0300

    setup: build, ts

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = [
+  {
+    mode: "development",
+    entry: "./src/electron.ts",
+    target: "electron-main",
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          include: /src/,
+          use: [{ loader: "ts-loader" }]
+        }
+      ]
+    },
+    output: {
+      path: __dirname + "/dist",
+      filename: "electron.js"
+    }
+  },
+  {
+    mode: "development",
+    entry: "./src/react.tsx",
+    target: "electron-renderer",
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          include: /src/,
+          use: [{ loader: "ts-loader" }]
+        }
+      ]
+    },
+    output: {
+      path: __dirname + "/dist",
+      filename: "react.js"
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html"
+      })
+    ]
+  }
+];
