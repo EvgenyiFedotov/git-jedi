5455ac86949e0f8d45470728ff899ff5293745f3
e.fedotov <fedotov1290@gmail.com>
2020-01-08 21:29:18 +0300

    feat: add begin files

---
+++ src/electron.ts
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  // Создаем окно браузера.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.webContents.openDevTools();
+
+  // и загрузить index.html приложения.
+  win.loadFile("../dist/index.html");
+}
+
+app.on("ready", createWindow);
