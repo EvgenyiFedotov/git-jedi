056c1069d138903b60fc7c45d2ce5d828d98f7b8
e.fedotov <fedotov1290@gmail.com>
2020-01-09 13:37:59 +0300

    feat: hide title window

--- src/electron.ts
+++ src/electron.ts
@@ -7,7 +7,8 @@ function createWindow() {
     height: 600,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    titleBarStyle: "hidden"
   });
 
   win.webContents.openDevTools();
