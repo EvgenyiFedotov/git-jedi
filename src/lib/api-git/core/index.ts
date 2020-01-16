b05c740849087ca05aa304c34c3de9e1aec404c4
e.fedotov <fedotov1290@gmail.com>
2020-01-16 22:24:49 +0300

    refacor: move stash

--- src/lib/api-git/core/index.ts
+++ src/lib/api-git/core/index.ts
@@ -3,3 +3,4 @@ export { log, Log, Commit, logSync } from "./log";
 export { showRef, showRefSync, ShowRef } from "./show-ref";
 export { revParse, revParseSync, RefParseOptions } from "./rev-parse";
 export { status, statusSync, StatusPath } from "./status";
+export { stash, stashSync, StashOptions } from "./stash";
---
+++ src/lib/api-git/core/stash.ts
@@ -0,0 +1,25 @@
+import { exec, execSync, BaseOptions } from "./exec";
+
+export interface StashOptions extends BaseOptions {
+  paths?: string[];
+}
+
+const createCommand = (options: StashOptions = {}): string => {
+  const { paths = [] } = options;
+
+  return `git stash push --keep-index --include-untracked -- ${paths.join(
+    " "
+  )}`;
+};
+
+export const stash = (options: StashOptions = {}) => {
+  const command = createCommand(options);
+
+  return exec(command, options.execOptions);
+};
+
+export const stashSync = (options: StashOptions = {}) => {
+  const command = createCommand(options);
+
+  return execSync(command, options.execOptions);
+};
