dc3aaa1bef90c1cf16923033908eb200d89bd6c2
e.fedotov <fedotov1290@gmail.com>
2020-01-17 13:28:44 +0300

    feat: add and setup antd

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,63 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-module.exports = [
-  {
-    mode: "development",
-    entry: "./src/electron.ts",
-    target: "electron-main",
-    devtool: "source-map",
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          include: /src/,
-          use: [{ loader: "ts-loader" }]
-        }
-      ]
-    },
-    output: {
-      path: __dirname + "/dist",
-      filename: "electron.js"
-    }
-  },
-  {
-    mode: "development",
-    entry: "./src/index.tsx",
-    target: "electron-renderer",
-    devtool: "source-map",
-    module: {
-      rules: [
-        {
-          test: /\.ts(x?)$/,
-          include: /src/,
-          use: ["ts-loader"]
-        }
-      ]
-    },
-    resolve: {
-      modules: ["node_modules", path.resolve(__dirname, "src")],
-      extensions: [".js", ".ts", ".json", ".tsx"]
-    },
-    output: {
-      path: __dirname + "/dist",
-      filename: "react.js"
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./public/index.html"
-      })
+const main = {
+  mode: "development",
+  entry: "./src/electron.ts",
+  target: "electron-main",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: /src/,
+        use: [{ loader: "ts-loader" }]
+      }
     ]
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "electron.js"
   }
-];
+};
+
+const renderer = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  target: "electron-renderer",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.ts(x?)$/,
+        include: /src/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  resolve: {
+    modules: ["node_modules", path.resolve(__dirname, "src")],
+    extensions: [".js", ".ts", ".json", ".tsx"]
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "react.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
+};
+
+module.exports = [main, renderer];
