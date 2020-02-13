51aea773eb93e3eaf2c60fd7f260088ea467c891
e.fedotov <fedotov1290@gmail.com>
2020-02-14 00:03:50 +0300

    feat(features/state-git): change editor

--- src/features/state-git/rebase/effects.ts
+++ src/features/state-git/rebase/effects.ts
@@ -1,13 +1,68 @@
 import { createEffect, sample, guard } from "effector";
-import { rebase as rebaseGit, ResetOptions } from "lib/api-git";
+import {
+  rebase as rebaseGit,
+  ResetOptions,
+  config as configGit,
+  ConfigOptions,
+} from "lib/api-git";
 import { pipeCommandToPromise } from "lib/pipe-command-promise";
 
 import { $runCommandOptions } from "../config";
 import { rebaseUp, abortRebase, rebaseEnd } from "./events";
 
+import { parseArgs } from "lib/parse-args";
+
+const CORE_EDITOR = "CORE_EDITOR";
+
 export const rebase = createEffect<ResetOptions, void>({
   handler: async (options) => {
+    await changeCoreEditor({
+      commandOptions: options.commandOptions,
+      spawnOptions: options.spawnOptions,
+    });
     await pipeCommandToPromise(rebaseGit(options));
+    await backCoreEditor({
+      commandOptions: options.commandOptions,
+      spawnOptions: options.spawnOptions,
+    });
+  },
+});
+
+const changeCoreEditor = createEffect<ConfigOptions, void>({
+  handler: async (options) => {
+    const pipe = configGit({ ...options, list: true }).next((config) => {
+      const pathEditor = getPathToEditor();
+      const coreEditor = config["core.editor"];
+
+      if (coreEditor && coreEditor !== pathEditor) {
+        localStorage.setItem(CORE_EDITOR, coreEditor);
+
+        configGit({
+          ...options,
+          value: ["core.editor", pathEditor],
+        });
+      }
+    });
+
+    await pipeCommandToPromise(pipe);
+  },
+});
+
+const backCoreEditor = createEffect<ConfigOptions, void>({
+  handler: async (options) => {
+    const pathEditor = getPathToEditor();
+    const coreEditor = localStorage.getItem(CORE_EDITOR);
+
+    if (coreEditor && coreEditor !== pathEditor) {
+      const pipe = configGit({
+        ...options,
+        value: ["core.editor", coreEditor],
+      });
+
+      await pipeCommandToPromise(pipe);
+    }
+
+    localStorage.removeItem(CORE_EDITOR);
   },
 });
 
@@ -40,3 +95,22 @@ sample({
   }),
   target: rebase,
 });
+
+function getPathToEditor(): string {
+  const appPath = parseArgs(process.argv.slice(2))["--app-path"];
+  let path;
+
+  switch (process.platform) {
+    case "darwin":
+      path = "git-editor-macos";
+      break;
+    case "linux":
+      path = "git-editor-linux";
+      break;
+    case "win32":
+      path = "git-editor-win.exe";
+      break;
+  }
+
+  return `${appPath}/${path}`;
+}
