fbd7fed4b6b4bb08062cdfe1e759ece06e346627
e.fedotov <fedotov1290@gmail.com>
2020-02-17 00:33:36 +0300

    refactor: remove server from main process

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,19 @@
-import { app, BrowserWindow, ipcMain } from "electron";
-import express from "express";
-import bodyParser from "body-parser";
+import { app, BrowserWindow } from "electron";
 
 let win: BrowserWindow;
 
 function createWindow() {
-  // Создаем окно браузера.
   win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
     },
-    titleBarStyle: "hidden",
+    // titleBarStyle: "hidden",
   });
 
   win.webContents.openDevTools();
-
-  // и загрузить index.html приложения.
   win.loadFile("../app/index.html");
 }
 
 app.on("ready", createWindow);
-
-const server = express();
-
-server.use(bodyParser.json());
-server.use(
-  bodyParser.urlencoded({
-    extended: true,
-  }),
-);
-
-server.post("/", (req, res) => {
-  const args: string[] = req.body.args;
-
-  ipcMain.once("rebase-response", (event, status) => {
-    res.send(status);
-  });
-  win.webContents.send("rebase-query", args);
-});
-
-server.listen(30000, function() {
-  console.log("Example app listening on port 30000!");
-});
