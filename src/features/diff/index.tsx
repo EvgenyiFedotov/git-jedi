9477548e04eccf9dcc77da622e6fa40e281d8ae5
e.fedotov <fedotov1290@gmail.com>
2020-02-19 18:50:14 +0300

    feat(features/diff): rename FileDiffLineV2 in FileDiffLine

--- src/features/diff/index.tsx
+++ src/features/diff/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { FileDiff, FileDiffChunk, FileDiffLineV2 } from "lib/api-git";
+import { FileDiff, FileDiffChunk, FileDiffLine } from "lib/api-git";
 import { Column, Row } from "ui";
 import { cyan, red, grey } from "@ant-design/colors";
 import { Branch } from "lib/branch";
@@ -15,13 +15,8 @@ export const Diff: React.FC<{ fileDiff: FileDiff | null }> = ({ fileDiff }) => {
 };
 
 const DiffChunk: React.FC<{ diffChunk: FileDiffChunk }> = ({ diffChunk }) => {
-  const lines = diffChunk.linesV2.map((diffLine, index) => (
-    <DiffLine
-      diffLine={diffLine}
-      key={index}
-      // numLineRemove={index + diffChunk.header.meta.remove.from}
-      // numLineAdd={index + diffChunk.header.meta.add.from}
-    />
+  const lines = diffChunk.lines.map((diffLine, index) => (
+    <DiffLine diffLine={diffLine} key={index} />
   ));
 
   return (
@@ -33,10 +28,7 @@ const DiffChunk: React.FC<{ diffChunk: FileDiffChunk }> = ({ diffChunk }) => {
 };
 
 const DiffLine: React.FC<{
-  diffLine: FileDiffLineV2;
-  // numLineRemove: number;
-  // numLineAdd: number;
-  // }> = ({ diffLine, numLineRemove, numLineAdd }) => {
+  diffLine: FileDiffLine;
 }> = ({ diffLine }) => {
   return (
     <Row style={{ flexWrap: "nowrap" }}>
@@ -82,11 +74,7 @@ const DiffLine: React.FC<{
   );
 };
 
-function joinChunks(
-  chunks: string[],
-  action: string,
-  diffLine: FileDiffLineV2,
-) {
+function joinChunks(chunks: string[], action: string, diffLine: FileDiffLine) {
   return chunks
     .filter((chunk) => {
       switch (chunk[0]) {
