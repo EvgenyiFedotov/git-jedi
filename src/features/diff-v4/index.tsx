ea7bbeb18d9f263567669af194b76e09486cefe8
e.fedotov <fedotov1290@gmail.com>
2020-02-25 00:03:48 +0300

    refactor(features/diff-v4): move to ui file

--- src/features/diff-v4/index.tsx
+++ src/features/diff-v4/index.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
 import { diffV2 } from "lib/api-git";
-import styled from "styled-components";
-import { cyan, red, grey, geekblue } from "@ant-design/colors";
+import { cyan, red } from "@ant-design/colors";
 import { Icon, Tooltip } from "antd";
 import { Branch } from "lib/branch";
+import { Change } from "diff";
 
-type Mode = "remove" | "add";
+import {
+  Separator,
+  Container,
+  InfoLines,
+  Lines,
+  Header,
+  AddButton,
+  Num,
+  Code,
+  Chunk,
+  getBgColor,
+} from "./ui";
+import { Mode } from "./types";
 
 export const DiffV4: React.FC<{
   diffFile: diffV2.DiffFile<diffV2.DiffLine[]> | null;
@@ -13,7 +25,25 @@ export const DiffV4: React.FC<{
 }> = ({ diffFile, mode }) => {
   if (!diffFile) return null;
 
-  const content = diffFile.chunks.reduce<{
+  const { infoLines, lines } = getContent(diffFile, mode);
+
+  return (
+    <Container>
+      <InfoLines>
+        <tbody>{infoLines}</tbody>
+      </InfoLines>
+      <Lines>
+        <tbody>{lines}</tbody>
+      </Lines>
+    </Container>
+  );
+};
+
+const getContent = (
+  diffFile: diffV2.DiffFile<diffV2.DiffLine[]>,
+  mode: Mode,
+) => {
+  return diffFile.chunks.reduce<{
     infoLines: React.ReactElement[];
     lines: React.ReactElement[];
   }>(
@@ -68,17 +98,6 @@ export const DiffV4: React.FC<{
       lines: [],
     },
   );
-
-  return (
-    <Container>
-      <InfoLines>
-        <tbody>{content.infoLines}</tbody>
-      </InfoLines>
-      <Lines>
-        <tbody>{content.lines}</tbody>
-      </Lines>
-    </Container>
-  );
 };
 
 const InfoLineHeaderChunk: React.FC<{
@@ -89,7 +108,7 @@ const InfoLineHeaderChunk: React.FC<{
     <Header>
       <AddButton>
         <Tooltip title="Add chunk">
-          <Icon type={mode === "remove" ? "minus" : "plus"} />
+          <Icon type="plus" />
         </Tooltip>
       </AddButton>
       <td></td>
@@ -99,11 +118,13 @@ const InfoLineHeaderChunk: React.FC<{
 
 const LineHeaderChunk: React.FC<{
   diffHeader: diffV2.DiffChunkHeader;
-}> = ({ diffHeader }) => {
+}> = ({ diffHeader: { meta, title } }) => {
+  const { remove, add } = meta;
+
   return (
     <Header>
       <td data-type="title">
-        {`@@ -${diffHeader.meta.remove.from},${diffHeader.meta.remove.length} +${diffHeader.meta.add.from},${diffHeader.meta.add.length} @@ ${diffHeader.title}`}
+        {`@@ -${remove.from},${remove.length} +${add.from},${add.length} @@ ${title}`}
       </td>
     </Header>
   );
@@ -115,23 +136,26 @@ const InfoLine: React.FC<{
   bgColor?: string;
 }> = ({ diffLine, mode, bgColor }) => {
   const modeLine = diffLine[mode];
+  const num = React.useMemo(
+    () =>
+      modeLine !== null
+        ? mode === "remove"
+          ? diffLine.removeNumLine
+          : diffLine.addNumLine
+        : " ",
+    [modeLine, mode, diffLine],
+  );
 
   return (
     <tr>
       <AddButton>
         <Branch if={modeLine !== null && diffLine.changed}>
           <Tooltip title="Add line">
-            <Icon type={mode === "remove" ? "minus" : "plus"} />
+            <Icon type="plus" />
           </Tooltip>
         </Branch>
       </AddButton>
-      <Num bgColor={bgColor}>
-        {modeLine !== null
-          ? mode === "remove"
-            ? diffLine.removeNumLine
-            : diffLine.addNumLine
-          : " "}
-      </Num>
+      <Num bgColor={bgColor}>{num}</Num>
     </tr>
   );
 };
@@ -141,29 +165,13 @@ const Line: React.FC<{
   diffLine: diffV2.DiffLine;
   bgColor?: string;
 }> = ({ mode, diffLine, bgColor }) => {
-  const changes =
-    diffLine.changed && diffLine.diff.length
-      ? diffLine.diff.reduce<React.ReactElement[]>((memo, change, index) => {
-          if (!change.removed && !change.added) {
-            memo.push(
-              <React.Fragment key={index}>{change.value}</React.Fragment>,
-            );
-          } else if (change.removed && mode === "remove") {
-            memo.push(
-              <Chunk key={index} bgColor={red[1]}>
-                {change.value}
-              </Chunk>,
-            );
-          } else if (change.added && mode === "add") {
-            memo.push(
-              <Chunk key={index} bgColor={cyan[1]}>
-                {change.value}
-              </Chunk>,
-            );
-          }
-          return memo;
-        }, [])
-      : diffLine[mode];
+  const changes = React.useMemo(
+    () =>
+      diffLine.changed && diffLine.diff.length
+        ? getChanges(diffLine.diff, mode)
+        : diffLine[mode],
+    [diffLine, mode],
+  );
 
   return (
     <tr>
@@ -172,85 +180,23 @@ const Line: React.FC<{
   );
 };
 
-const Separator: React.FC = () => (
-  <tr>
-    <td></td>
-  </tr>
-);
-
-const Container = styled.div`
-  display: flex;
-  flex: 1;
-  width: 100%;
-  flex-wrap: nowrap;
-`;
-
-const Table = styled.table`
-  font-family: monospace;
-  font-size: small;
-  display: block;
-
-  tr {
-    height: 21px;
-
-    td {
-      white-space: nowrap;
-      padding: 0 4px;
+const getChanges = (diff: Change[], mode: Mode) => {
+  return diff.reduce<React.ReactElement[]>((memo, change, index) => {
+    if (!change.removed && !change.added) {
+      memo.push(<React.Fragment key={index}>{change.value}</React.Fragment>);
+    } else if (change.removed && mode === "remove") {
+      memo.push(
+        <Chunk key={index} bgColor={red[1]}>
+          {change.value}
+        </Chunk>,
+      );
+    } else if (change.added && mode === "add") {
+      memo.push(
+        <Chunk key={index} bgColor={cyan[1]}>
+          {change.value}
+        </Chunk>,
+      );
     }
-  }
-`;
-
-const InfoLines = styled(Table)``;
-
-const Lines = styled(Table)`
-  width: 100%;
-  overflow-x: auto;
-  white-space: nowrap;
-  position: relative;
-`;
-
-const Header = styled.tr`
-  background-color: ${geekblue[0]};
-  font-weight: bold;
-
-  td[data-type="title"] {
-    width: 100vw;
-  }
-`;
-
-const AddButton = styled.td`
-  cursor: pointer;
-  color: ${grey[2]};
-`;
-
-const Num = styled.td<{ bgColor?: string }>`
-  color: ${grey[0]};
-  text-align: right;
-  background-color: ${({ bgColor }) => bgColor};
-`;
-
-const Code = styled.td<{ bgColor?: string }>`
-  width: 100%;
-  white-space: pre !important;
-  background-color: ${({ bgColor }) => bgColor};
-`;
-
-const Chunk = styled.span<{ bgColor?: string }>`
-  background-color: ${({ bgColor }) => bgColor};
-  border-radius: 3px;
-  padding: 3px 2px;
-`;
-
-function getBgColor(diffLine: diffV2.DiffLine, mode: Mode) {
-  const modeLine = diffLine[mode];
-
-  if (!!modeLine && diffLine.changed) {
-    if (diffLine.remove && mode === "remove") {
-      return red[0];
-    }
-
-    if (diffLine.add && mode === "add") {
-      return cyan[0];
-    }
-  }
-}
+    return memo;
+  }, []);
+};
