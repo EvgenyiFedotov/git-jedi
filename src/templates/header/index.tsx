01fd87b57aed56d24e172300bbca193cb48a9fed
e.fedotov <fedotov1290@gmail.com>
2020-03-01 19:00:47 +0300

    feat(templates/header): add visible flag for remote add field

--- src/templates/header/index.tsx
+++ src/templates/header/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import styled from "styled-components";
-
 import { Path } from "features/path";
 import { CurrentBrunch } from "features/current-branch";
 import { CreateBranch } from "features/create-branch";
@@ -9,18 +8,35 @@ import { DiffCommits } from "features/diff-commits";
 import { Commands } from "features/commands";
 import { Config as GitConfig } from "features/git-config";
 import { RemoteAdd } from "features/remote-add";
+import { useStore } from "effector-react";
+import { Branch } from "lib/branch";
+
+import { $visibleRemoteUrl, changeVisibleRemoteUrl } from "./model";
 
 export const Header: React.FC = () => {
+  const visibleRemoteUrl = useStore($visibleRemoteUrl);
+
+  const closeRemoteUrl = React.useCallback(
+    () => changeVisibleRemoteUrl.hide(),
+    [],
+  );
+
   return (
     <Container>
       <Path />
       <Row>
-        <Commands />
-        <DiffCommits />
-        <CreateBranch />
-        <CurrentBrunch />
-        <GitConfig />
-        <RemoteAdd />
+        <Branch if={visibleRemoteUrl}>
+          <>
+            <RemoteAdd onClose={closeRemoteUrl} />
+          </>
+          <>
+            <Commands />
+            <DiffCommits />
+            <CreateBranch />
+            <CurrentBrunch />
+            <GitConfig />
+          </>
+        </Branch>
       </Row>
     </Container>
   );
