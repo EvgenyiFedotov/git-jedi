6f321418892463344461fa2f9688130e7530cbc1
e.fedotov <fedotov1290@gmail.com>
2020-03-03 19:14:06 +0300

    refactor(lib/pipe-v2): move from 'pipe-v3'

---
+++ src/lib/pipe-v2/index.ts
@@ -0,0 +1,92 @@
+import { v4 as uuid } from "uuid";
+
+type Callback<Value, Result> = (
+  value: Value,
+  action?: string,
+) => Result | Pipe<Result> | Promise<Result | Pipe<Result>>;
+
+export interface Pipe<Value> {
+  listen: <Result>(
+    callback: Callback<Value, Result>,
+    key?: string,
+  ) => Pipe<Value>;
+  next: <Result>(
+    callback: Callback<Value, Result>,
+    key?: string,
+  ) => Pipe<Result>;
+  resolve: (value: Value, action?: string) => Promise<Map<string, any>>;
+  resolvedStore: () => Map<string, any[]>;
+}
+
+const isPipe = <Value>(x: any): x is Pipe<Value> => {
+  return typeof x === "object" && !!x.next && !!x.resolve;
+};
+
+export interface CreatePipeOptions {
+  saveResolveResult?: boolean;
+}
+
+export const createPipe = <Value>(options: CreatePipeOptions = {}) => {
+  const { saveResolveResult = false } = options;
+
+  const callbacks: Map<
+    string,
+    (value: Value, action?: string) => any
+  > = new Map();
+  const pipes: Map<string, Pipe<any>> = new Map();
+  const resolvedStore: Map<string, any[]> = new Map();
+
+  const result: Pipe<Value> = {
+    listen: (callback, key: string = uuid()) => {
+      callbacks.set(key, callback);
+
+      return result;
+    },
+
+    next: <Result>(callback: Callback<Value, Result>, key: string = uuid()) => {
+      const pipe = createPipe<Result>();
+
+      result.listen(callback, key);
+      pipes.set(key, pipe);
+
+      return pipe;
+    },
+
+    resolve: async (value: Value, action?: string) => {
+      const callbackArr = Array.from(callbacks);
+      const resolveResult: Map<string, any> = new Map();
+
+      if (!saveResolveResult) {
+        resolvedStore.clear();
+      }
+
+      for (let index = 0; index < callbackArr.length; index += 1) {
+        const [key, callback] = callbackArr[index];
+        const result = await callback(value, action);
+        const pipe = pipes.get(key);
+
+        resolveResult.set(key, result);
+
+        if (resolvedStore.has(key)) {
+          resolvedStore.get(key)?.push(result);
+        } else {
+          resolvedStore.set(key, [result]);
+        }
+
+        if (pipe) {
+          if (isPipe(result)) {
+            result.next(pipe.resolve, key);
+          } else {
+            pipe.resolve(result, action);
+          }
+        }
+      }
+
+      return resolveResult;
+    },
+
+    resolvedStore: () => resolvedStore,
+  };
+
+  return result;
+};
---
+++ src/lib/pipe-v2/test.ts
@@ -0,0 +1,136 @@
+import { createPipe, Pipe } from ".";
+
+test("listen", () => {
+  let result: string = "";
+  const pipe = createPipe<string>().listen((value) => (result = value));
+
+  pipe.resolve("123");
+
+  expect(result).toBe("123");
+});
+
+test("listen with key", async () => {
+  const pipe = createPipe<string>().listen((value) => [value], "key-action");
+
+  const resultResolve = await pipe.resolve("123");
+
+  expect(resultResolve.has("key-action")).toBe(true);
+  expect(resultResolve.get("key-action")).toEqual(["123"]);
+});
+
+test("resolve with action", async () => {
+  const pipe = createPipe<string>().listen(
+    (value, action) => (action === "link" ? `${value}-${value}` : [value]),
+    "key-action",
+  );
+
+  const resultResolve1 = await pipe.resolve("123");
+
+  expect(resultResolve1.has("key-action")).toBe(true);
+  expect(resultResolve1.get("key-action")).toEqual(["123"]);
+
+  const resultResolve2 = await pipe.resolve("asd", "link");
+
+  expect(resultResolve2.has("key-action")).toBe(true);
+  expect(resultResolve2.get("key-action")).toEqual("asd-asd");
+});
+
+test("next", async () => {
+  const pipeRoot = createPipe<number>();
+  let pupeSub1Result: string = "";
+  let pupeSub1Action: string | undefined;
+
+  pipeRoot
+    .next((value) => [value, value, value], "pipe-sub-1")
+    .next((value, action) => {
+      pupeSub1Result = value.join("-");
+      pupeSub1Action = action;
+      return pupeSub1Result;
+    }, "pipe-sub-1-1");
+
+  pipeRoot.next((value) => value * 10, "pipe-sub-2");
+
+  const pipeRootResult = await pipeRoot.resolve(123, "stream");
+
+  expect(pipeRootResult.get("pipe-sub-1")).toEqual([123, 123, 123]);
+  expect(pipeRootResult.get("pipe-sub-2")).toEqual(1230);
+  expect(pupeSub1Result).toBe("123-123-123");
+  expect(pupeSub1Action).toBe("stream");
+});
+
+test("next subpipe", async () => {
+  const pipeRoot = createPipe<string>();
+  const subpipe = createPipe<number>();
+  let subpipeValue: number = -1;
+
+  pipeRoot.next(() => subpipe).listen((value) => (subpipeValue = value));
+
+  await pipeRoot.resolve("asd");
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      subpipe.resolve(123);
+      resolve();
+    }, 1000),
+  );
+
+  expect(subpipeValue).toBe(123);
+});
+
+test("next async", async () => {
+  const pipeRoot = createPipe<string>();
+  let subpipe: Pipe<number>;
+  let subpipeValue: number = -1;
+
+  pipeRoot.listen(async (value) => `@${value}`, "listen-1");
+  pipeRoot.listen(async (value) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return `@${value}@`;
+  }, "listen-2");
+  pipeRoot.next(async (value) => [value, value], "subpipe-1");
+  pipeRoot.next(async (value) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return [value, value, value];
+  }, "subpipe-2");
+  pipeRoot
+    .next(async () => {
+      subpipe = createPipe<number>();
+      return subpipe;
+    })
+    .listen((value) => (subpipeValue = value));
+
+  const result = await pipeRoot.resolve("asd", "stream");
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      subpipe.resolve(123);
+      resolve();
+    }, 1000),
+  );
+
+  expect(result.get("listen-1")).toBe("@asd");
+  expect(result.get("listen-2")).toBe("@asd@");
+  expect(result.get("subpipe-1")).toEqual(["asd", "asd"]);
+  expect(result.get("subpipe-2")).toEqual(["asd", "asd", "asd"]);
+  expect(subpipeValue).toBe(123);
+});
+
+test("resolvedStore", async () => {
+  const pipe = createPipe<string>();
+
+  pipe.listen((value) => `${value}-${value}`, "listener");
+
+  await pipe.resolve("asd");
+  await pipe.resolve("qwe");
+
+  expect(pipe.resolvedStore().get("listener")).toEqual(["qwe-qwe"]);
+});
+
+test("resolvedStore with save", async () => {
+  const pipe = createPipe<string>({ saveResolveResult: true });
+
+  pipe.listen((value) => `${value}-${value}`, "listener");
+
+  await pipe.resolve("asd");
+  await pipe.resolve("qwe");
+
+  expect(pipe.resolvedStore().get("listener")).toEqual(["asd-asd", "qwe-qwe"]);
+});
