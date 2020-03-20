b2727657ce8135bc02a1bc13f36e10d030153404
e.fedotov <fedotov1290@gmail.com>
2020-03-20 13:44:46 +0300

    refactor(lib/v2/file-connector): add

---
+++ src/lib/v2/file-connector/index.ts
@@ -0,0 +1,49 @@
+import { FileWatcher } from "lib/v2/file-watcher";
+import { createPipe } from "lib/pipe";
+import * as fsPromise from "lib/v2/fs-promise";
+
+export type Options = {
+  watcher: FileWatcher;
+  id: string;
+};
+
+export type Data = {
+  id: string;
+  message: any;
+};
+
+export type FileConnector = {
+  watch: (cb: (data: Data) => void) => void;
+  send: (message: any) => void;
+};
+
+export const createFileConnector = (options: Options): FileConnector => {
+  const pipe = createPipe<Data>();
+
+  options.watcher.pipe().listen(({ action, file }) => {
+    const content = file.toString();
+
+    try {
+      const data = JSON.parse(content);
+
+      if (data.id !== options.id) {
+        pipe.resolve(data);
+      }
+    } catch (error) {
+      // pass
+    }
+  });
+
+  const fileConnector: FileConnector = {
+    watch: (cb) => {
+      pipe.listen(cb);
+    },
+    send: (message) => {
+      const nextData = JSON.stringify({ id: options.id, message });
+
+      fsPromise.writeFile(options.watcher.path(), nextData);
+    },
+  };
+
+  return fileConnector;
+};
---
+++ src/lib/v2/file-connector/test.ts
@@ -0,0 +1,28 @@
+import { createFileWatcher } from "lib/v2/file-watcher";
+import * as fs from "fs";
+import { v4 as uuid } from "uuid";
+
+import { createFileConnector, Data } from "./index";
+
+const path = `${__dirname}/TEST`;
+
+test("connect", async () => {
+  const watcher = await createFileWatcher({ path, watch: true });
+  const connector1 = createFileConnector({ watcher, id: "connector-1" });
+  const connector2 = createFileConnector({ watcher, id: "connector-2" });
+
+  const callback = new Promise<Data>((resolve) => {
+    connector1.watch(resolve);
+  });
+  const message = uuid();
+
+  fs.writeFileSync(path, "");
+  connector2.send(message);
+
+  const result = await callback;
+
+  expect(result.id).toBe("connector-2");
+  expect(result.message).toBe(message);
+
+  fs.unlinkSync(path);
+});
