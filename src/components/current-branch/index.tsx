263aeeb18bc0f1bc1eebc921b45716aa1d315e1d
e.fedotov <fedotov1290@gmail.com>
2020-04-07 16:34:39 +0300

    refactor(move-to-model): move ButtonSettings

--- src/components/current-branch/index.tsx
+++ src/components/current-branch/index.tsx
@@ -2,25 +2,45 @@ import * as React from "react";
 import styled from "styled-components";
 import { LinkBlock } from "ui";
 import { useStore } from "effector-react";
-import { Spin, Tooltip } from "antd";
+import { Spin, Tooltip, Drawer } from "antd";
 import { BranchesOutlined } from "@ant-design/icons";
+import { createVisible } from "lib/added-effector/visible";
+import { BranchList } from "components/branch-list";
 
 import { currentBranch } from "model";
 
-export const CurrentBranch: React.FC<{ onClick?: () => void }> = ({
-  onClick,
-}) => {
+export const visibleBranchList = createVisible();
+
+export const CurrentBranch: React.FC = () => {
   const value = useStore(currentBranch.$currentBranch);
 
   return (
     <Spin size="small" spinning={!currentBranch}>
       <Tooltip title="Current branch">
-        <Container onClick={onClick}>
+        <Container onClick={() => visibleBranchList.show()}>
           <BranchesOutlined />
           {value}
         </Container>
       </Tooltip>
+      <DrawerBranchList />
     </Spin>
+  );
+};
+
+const DrawerBranchList: React.FC = () => {
+  const visible = useStore(visibleBranchList.$value);
+
+  return (
+    <Drawer
+      title="Branch list"
+      closable={false}
+      visible={visible}
+      onClose={() => visibleBranchList.hide()}
+      placement="right"
+      width="460px"
+    >
+      <BranchList />
+    </Drawer>
   );
 };
 
