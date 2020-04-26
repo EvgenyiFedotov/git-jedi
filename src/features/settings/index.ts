77cb8525cc039dcbc7c6049a0458de4c6872b9ac
e.fedotov <fedotov1290@gmail.com>
2020-04-26 14:06:14 +0300

    chore(features/settings): export

--- src/features/settings/index.ts
+++ src/features/settings/index.ts
@@ -3,3 +3,4 @@ import "./model/init";
 export { Settings } from "./components/settings";
 export { WordDir } from "./components/work-dir";
 export { SettingsButton } from "./components/settings-button";
+export { $workDir } from "./model";
